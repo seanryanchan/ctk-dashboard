ec751953974e12fc4bdfce0357a9033d4fed8f5b
Sean Ryan Chan <seanryachan@gmail.com>
2019-01-30 16:34:22 +0800

    FIxed unit price front end for products index html

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -78,6 +78,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:brand, :product_type, :quantity, :backlogged, :brand_query)
+      params.require(:product).permit(:brand, :product_type, :quantity, :unit_price, :brand_query)
     end
 end
